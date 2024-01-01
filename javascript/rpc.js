1ebb92e12d8142d16506244ed317267c0c966902
azolfagharir <azolfagharir@gmail.com>
2024-01-01 18:10:24 +0330

    a dependency was added

--- javascript/rpc.js
+++ javascript/rpc.js
@@ -1,6 +1,22 @@
+
+import Swal from('sweetalert2');
+
+Swal.fire({
+    title: 'Error!',
+    text: 'Do you want to continue',
+    icon: 'error',
+    confirmButtonText: 'Cool'
+  });
 var user_choice;
 let computer_choice;
 var result;
+var sum = 0;
+
+let score ={
+    losses : 0,
+    wins : 0,
+    ties : 0
+}
 function randomchoice(){
     var random_choice = Math.random();
     if(random_choice <= 0.33){
@@ -15,6 +31,7 @@ function randomchoice(){
 }
 
 function play(user_choice){
+    if(sum < 11){
     randomchoice(); // Call randomchoice() here
     if(user_choice === 'rock' && computer_choice === 'rock'){
         result = 'Tie';
@@ -45,7 +62,25 @@ function play(user_choice){
     else if(user_choice === 'scissors' && computer_choice === 'paper'){
         result = 'you won';
     }
-     document.getElementById("demo").innerHTML = result;
-        
-    
+    scores();
+    document.getElementById("demo").innerHTML = `you chose: ${user_choice} and computer chose: ${computer_choice} <br> <br> ${result} <br><br> wins:${score.wins} <br><br> losses:${score.losses} <br><br> Ties:${score.ties}`;
+    sum = score.losses + score.wins + score.ties;
+    }
+   /* else if(){
+
+    }*/
 }
+        
+    function scores(){
+        if(result === 'you won'){
+            score.wins++;
+        }
+        else if(result==='you lost'){
+            score.losses++;
+        }
+        else if(result === 'Tie'){
+            score.ties++;
+        }
+    }
+
+
